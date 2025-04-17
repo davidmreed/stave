7aa9fec615fd939be32a6d906e02c4cb52dd9ff0
David Reed <david@ktema.org>
2025-04-16 22:39:35 -0600

    Add sqlite to flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
@@ -9,7 +9,7 @@
       let pkgs = import nixpkgs { system = system; config.allowUnfree = true; }; in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ python313 git just ruff uv basedpyright railway ];
+          packages = with pkgs; [ python313 git just ruff uv basedpyright railway sqlite ];
         };
       }
     );
