bb2b2624755894b0640bcb62f6447e2d45006d80
David Reed <david@ktema.org>
2025-05-26 15:23:51 -0600

    Admin update

--- stave/admin.py
+++ stave/admin.py
@@ -5,6 +5,18 @@ from . import models
 
 admin.site.login = secure_admin_login(admin.site.login)
 
+
+class CrewAssignmentInline(admin.TabularInline):
+    model = models.CrewAssignment
+    list_display = ("event", "role", "user")
+
+
+@admin.register(models.Crew)
+class CrewAdmin(admin.ModelAdmin):
+    inlines = [CrewAssignmentInline]
+    list_display = ("event", "role_group", "name", "kind")
+
+
 for model in [
     models.User,
     models.League,
