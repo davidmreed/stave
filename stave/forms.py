cceaebc7f18a807bbd8415ba561a5185118ebd4b
David Reed <david@ktema.org>
2025-08-10 22:20:03 -0600

    Fix the SeparatedJSONListField once and for all

--- stave/forms.py
+++ stave/forms.py
@@ -46,15 +46,18 @@ class SeparatedJSONListField(forms.JSONField):
 
         return value
 
-    def to_python(self, value: str) -> str:
+    def to_python(self, value: str | list[str] | None) -> list[str] | None:
         if value:
-            python_value = [
-                v.strip() for v in value.strip().replace("\r", "\n").split("\n") if v
-            ]
-        else:
-            python_value = []
+            if isinstance(value, str):
+                python_value = [
+                    v.strip()
+                    for v in value.strip().replace("\r", "\n").split("\n")
+                    if v
+                ]
+            elif isinstance(value, list):
+                python_value = value
 
-        return super().to_python(json.dumps(python_value))
+            return python_value
 
 
 class MultipleChoiceOrOtherWidget(forms.MultiWidget):
