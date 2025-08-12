bad4f8d4dcf980fe3e40d3b3709f10ad42c13542
David Reed <david@ktema.org>
2025-08-11 21:37:58 -0600

    uv-check does not seem to work

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
               nixpkgs-fmt.enable = true;
               ruff.enable = true;
               ruff-format.enable = true;
-              uv-check.enable = true;
             };
           };
         };
