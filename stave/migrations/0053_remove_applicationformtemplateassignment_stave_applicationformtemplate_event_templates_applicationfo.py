451d1206e4ca6dd473f60de628e0667a7727729f
David Reed <david@ktema.org>
2025-11-18 12:19:01 -0700

    Try using SeparateDatabaseAndState

--- stave/migrations/0053_remove_applicationformtemplateassignment_stave_applicationformtemplate_event_templates_applicationfo.py
+++ stave/migrations/0053_remove_applicationformtemplateassignment_stave_applicationformtemplate_event_templates_applicationfo.py
@@ -9,9 +9,19 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveConstraint(
-            model_name="applicationformtemplateassignment",
-            name="stave_applicationformtemplate_event_templates_applicationformtemplate_id_eventtemplate_id_f2fba8ea_uniq",
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL("""
+                DROP INDEX IF EXISTS stave_applicationformtem_applicationformtemplate__f2fba8ea_uniq;
+                DROP INDEX IF EXISTS stave_applicationformtemplate_event_templates_applicationformtemplate_id_eventtemplate_id_f2fba8ea_uniq;
+                """)
+            ],
+            state_operations=[
+                migrations.RemoveConstraint(
+                    model_name="applicationformtemplateassignment",
+                    name="stave_applicationformtemplate_event_templates_applicationformtemplate_id_eventtemplate_id_f2fba8ea_uniq",
+                )
+            ],
         ),
         migrations.RenameField(
             model_name="applicationformtemplateassignment",
