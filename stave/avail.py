a4a7cbee058d6aa9bc538db19f89e061a439cc36
David Reed <david@ktema.org>
2025-11-22 23:49:53 -0700

    WIP

--- stave/avail.py
+++ stave/avail.py
@@ -334,6 +334,13 @@ class AvailabilityManager:
         )
 
     @functools.cache
+    def get_swappable_applications(self, crew: models.Crew, game: models.Game | None, role: models.Role) -> list[models.Application]:
+        """To be swappable, all overlapping assignments have to be in Role Groups managed on the same
+        Application Form, and cannot be part of a static crew. If we swapped out of a static crew,
+        we'd have no way to mark the slot empty."""
+        ...
+
+    @functools.cache
     def get_available_applications(
         self, crew: models.Crew, game: models.Game | None, role: models.Role
     ) -> list[models.Application]:
