0c3dd4f238be35275dd19c263914fc54c31a071f
David Reed <david@ktema.org>
2025-11-29 21:27:43 -0700

    Add tests

--- tests/test_avail.py
+++ tests/test_avail.py
@@ -1,7 +1,111 @@
-from tests.factories import ApplicationFactory
+from tests.factories import ApplicationFactory, CrewFactory
+from datetime import datetime, timedelta, timezone
+from stave.avail import AvailabilityManager, UserAvailabilityEntry, ConflictKind
+from pytest import fixture
+from stave import models
 
-from stave.avail import AvailabilityManager
+@fixture
+def existing_entry(db):
+    start_time = datetime.now(tz=timezone.utc)
+    end_time = start_time + timedelta(hours=2)
+    return UserAvailabilityEntry(
+        crew = CrewFactory(kind=models.CrewKind.OVERRIDE_CREW),
+        start_time=start_time,
+        end_time=end_time,
+        exclusive=True
+    )
+@fixture
+def existing_static_crew_entry(db):
+    start_time = datetime.now(tz=timezone.utc)
+    end_time = start_time + timedelta(hours=2)
+    return UserAvailabilityEntry(
+        crew = CrewFactory(kind=models.CrewKind.GAME_CREW),
+        start_time=start_time,
+        end_time=end_time,
+        exclusive=True
+    )
 
+def test_user_availability_entry__simple_overlap(existing_entry):
+    assert existing_entry.overlaps(UserAvailabilityEntry(
+        crew=existing_entry.crew,
+        start_time=existing_entry.start_time,
+        end_time=existing_entry.end_time,
+        exclusive=True
+    ), set()) == ConflictKind.SWAPPABLE_CONFLICT
+def test_user_availability_entry__simple_overlap_nonexclusive(existing_entry):
+    assert existing_entry.overlaps(UserAvailabilityEntry(
+        crew=existing_entry.crew,
+        start_time=existing_entry.start_time,
+        end_time=existing_entry.end_time,
+        exclusive=False
+    ), set()) == ConflictKind.NONE
+
+def test_user_availability_entry__simple_overlap_other_role_group(existing_entry):
+    assert existing_entry.overlaps(UserAvailabilityEntry(
+        crew=CrewFactory(kind=models.CrewKind.OVERRIDE_CREW),
+        start_time=existing_entry.start_time,
+        end_time=existing_entry.end_time,
+        exclusive=True
+    ), set()) == ConflictKind.NON_SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__swappable_overlap_other_role_group(existing_entry):
+    other_crew = CrewFactory(kind=models.CrewKind.OVERRIDE_CREW)
+    assert existing_entry.overlaps(UserAvailabilityEntry(
+        crew=other_crew,
+        start_time=existing_entry.start_time,
+        end_time=existing_entry.end_time,
+        exclusive=False
+    ),set([other_crew.role_group])) == ConflictKind.SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__partial_overlap(existing_entry):
+    assert existing_entry.overlaps(UserAvailabilityEntry(
+        crew=existing_entry.crew,
+        start_time=existing_entry.start_time + timedelta(hours=1),
+        end_time=existing_entry.end_time,
+        exclusive=True
+    ), set()) == ConflictKind.SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__event_crews():
+    ...
+
+def test_user_availability_entry__static_crews(existing_static_crew_entry):
+    assert existing_static_crew_entry.overlaps(UserAvailabilityEntry(
+        crew=existing_static_crew_entry.crew,
+        start_time=None,
+        end_time=None,
+        exclusive=True
+    ), set()) == ConflictKind.SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__static_crews_nonexclusive(existing_static_crew_entry):
+    assert existing_static_crew_entry.overlaps(UserAvailabilityEntry(
+        crew=existing_static_crew_entry.crew,
+        start_time=None,
+        end_time=None,
+        exclusive=False
+    ), set()) == ConflictKind.NONE
+
+def test_user_availability_entry__static_crews_other_role_group(existing_static_crew_entry):
+    assert existing_static_crew_entry.overlaps(UserAvailabilityEntry(
+        crew=CrewFactory(kind=models.CrewKind.GAME_CREW),
+        start_time=None,
+        end_time=None,
+        exclusive=True
+    ), set()) == ConflictKind.NON_SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__static_crews_swappable_role_group(existing_static_crew_entry):
+    other_crew = CrewFactory(kind=models.CrewKind.GAME_CREW)
+    assert existing_static_crew_entry.overlaps(UserAvailabilityEntry(
+        crew=CrewFactory(kind=models.CrewKind.GAME_CREW),
+        start_time=None,
+        end_time=None,
+        exclusive=True
+    ), set([other_crew.role_group])) == ConflictKind.SWAPPABLE_CONFLICT
+
+def test_user_availability_entry__override_of_static_crew():
+    ...
+
+def test_user_availability_entry__non_meaningful():
+    ...
 
 def test_availability_manager__applications(tournament):
     form = tournament.application_forms.get(slug="apply-nso-so")
