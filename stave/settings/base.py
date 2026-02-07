429f9cfdcd8d458d8dff47a11cdb05470f053e7f
Mike Fiedler <miketheman@gmail.com>
2026-02-07 11:20:35 -0500

    test: disable coverage reporting on env-specifics
    When running tests, we rarely set the env var to a production value.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- stave/settings/base.py
+++ stave/settings/base.py
@@ -33,7 +33,7 @@ LOGGING = {
 
 # Sentry initialization should happen before importing Django apps/middleware
 # to ensure errors are captured correctly.
-if sentry_dsn := os.environ.get("SENTRY_DSN"):
+if sentry_dsn := os.environ.get("SENTRY_DSN"):  # pragma: no cover
     # Initialize Sentry for production error tracking
     sentry_sdk.init(
         dsn=sentry_dsn,
