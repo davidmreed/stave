357a07fc16fe2fbf56f0c3cbec142487868c9e9c
David Reed <david@ktema.org>
2026-05-09 12:57:09 -0600

    Make Postgres for local dev easier to use (#259)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       {
         devShells.default = pkgs.mkShell {
           NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.glibc
+            pkgs.glibc pkgs.libz
           ];
           SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt";
           packages = with pkgs; [ git just uv railway sqlite mdbook mdbook-mermaid ];
