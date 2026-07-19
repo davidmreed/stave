72f92dd3d860a4a38942eeda02e5d6be5a67c2c6
David Reed <david@ktema.org>
2026-07-19 17:59:14 -0600

    Format and fix

--- stave/views.py
+++ stave/views.py
@@ -2148,7 +2148,9 @@ class CrewBuilderDetailView(LoginRequiredMixin, views.View):
             except UserNotAvailableException:
                 messages.info(
                     request,
-                    _("The selected user is not available for the chosen slot."),
+                    gettext_lazy(
+                        "The selected user is not available for the chosen slot."
+                    ),
                 )
 
             # Redirect the user to the Crew Builder for this crew
