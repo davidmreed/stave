87eed80dbb2f87c4e0043daf77c8f627b5e032da
David Reed <david@ktema.org>
2026-07-19 11:54:35 -0600

    Handle the SSL path correctly in Nix flake

--- flake.nix
+++ flake.nix
@@ -8,11 +8,13 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { system = system; }; in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShellNoCC {
           NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
             pkgs.glibc pkgs.libz
           ];
-          SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt";
+          shellHook = ''
+            export SSL_CERT_FILE="/etc/static/ssl/certs/ca-bundle.crt"
+          '';
           packages = with pkgs; [ git just uv railway sqlite mdbook mdbook-mermaid ];
         };
       }
